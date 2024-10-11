22eca6ea715593e997a63a2a846f25a015508112
baptiste.freminet <baptiste.freminet@lycee-elie-vinet.org>
2024-10-11 09:53:28 +0200

    début JS et switch dark-light

--- js/styles.js
+++ js/styles.js
@@ -1,4 +1,25 @@
-var button=document.getElementById("btn_theme");
-button.onclick=function(){
-    alert('hello world');
-};
+var switch_theme_sun=document.getElementById("icon_light");
+var switch_theme_moon=document.getElementById("icon_dark");
+switch_theme_sun.onclick=function(){change_theme(switch_theme_sun);};
+switch_theme_moon.onclick=function(){change_theme(switch_theme_moon);};
+
+
+function change_theme(icon){
+    icon.style.display = "none"
+
+    next_theme=getLocalData()=="dark"?'light':'dark'
+
+    document.getElementById('icon_'+next_theme).style.display = "show"
+    saveLocalData(next_theme)
+    // récupère valeur du theme dans la base web
+    // l'inverse
+    // hide icon + affiche nouvel icon
+    // change theme page (bg body + color texte / icon + bascule image si index.html)
+}
+
+function saveLocalData(theme){
+    localStorage.setItem('theme',theme)
+}
+function getLocalData(){
+    return localStorage.getItem('theme')
+}
